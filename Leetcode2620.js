109168b7710b520cc4b1fdac94863c5aa50a3571
John <tim@cruz-family.ws>
2023-05-11 10:29:17 -0700

    one problem

---
+++ Leetcode2620.js
@@ -0,0 +1,15 @@
+// 2620. Counter
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+var createCounter = function(n) {
+    return function() {
+        return n++
+    };
+};
+
+let counter = createCounter(5)
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
