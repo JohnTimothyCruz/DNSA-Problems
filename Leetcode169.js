cbef06dc1c743bf6c7632766bf68fd096c2720f0
John <tim@cruz-family.ws>
2023-05-17 09:16:25 -0700

    one problem

---
+++ Leetcode169.js
@@ -0,0 +1,25 @@
+// 169. Majority Element
+
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+var majorityElement = function(nums) {
+    let majority = 0
+    let count = 0
+
+    for (const num of nums) {
+        if (count === 0) {
+            majority = num
+        }
+
+        if (majority === num) {
+            count++
+        } else {
+            count--
+        }
+    }
+
+    return majority
+};
+
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]))
