2374507506e2d13bcfdf98a36be93bc4504baaff
John <tim@cruz-family.ws>
2023-05-22 11:32:23 -0700

    leetcode 268

---
+++ Leetcode268.js
@@ -0,0 +1,17 @@
+// 268. Missing Number
+
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+var missingNumber = function(nums) {
+    let sumAndLength = nums.length
+    let sum = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        sumAndLength += i
+        sum += nums[i]
+    }
+
+    return sumAndLength - sum
+};
+
+console.log(missingNumber([3, 0, 1]))
