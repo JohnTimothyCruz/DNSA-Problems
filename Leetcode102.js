723fdadd3a71dfb55ffe8bbb24ff2edfa450b86d
John <tim@cruz-family.ws>
2023-07-03 09:12:28 -0700

    leetcode 102

--- Leetcode102.js
+++ Leetcode102.js
@@ -24,3 +24,26 @@ var levelOrder = function (root) {
 
     return res;
 };
+
+// Making binary tree here
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null
+    };
+};
+
+let node1 = new TreeNode(3);
+let node2 = new TreeNode(9);
+let node3 = new TreeNode(20);
+let node4 = new TreeNode(15);
+let node5 = new TreeNode(7);
+
+node1.left = node2;
+node1.right = node3;
+node3.left = node4;
+node3.right = node5;
+
+// Test here
+console.log(levelOrder(node1))
